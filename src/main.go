e4975f01535f63d108bfbc09ce77f65f18c6b762
Prem Jagadeesh <prem.jagadeesh1@gmail.com>
2022-07-20 19:06:39 +0200

    tokens

--- src/main.go
+++ src/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("test");
+	
+    var appleDeveloperToken = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRSVUM3NzdEWkwifQ.eyJpYXQiOjE2NTgzMzYzNzksImV4cCI6MTY3Mzg4ODM3OSwiaXNzIjoiM1M1OVVGV1A4WSJ9.ywmXqlzN700OszGHLXePHOk2ifJ3EFWk_mF5XZT8w9bPgK9epenRO62tbpjdVMNgu6p6Hio6095Wig73_3xtwA";
+    
+    // Currently personal token
+    var musicUserToken = "AmIKSqcLaVo8ziS52+UVu6Qf+OW8vg8qXAhbpRi/+eh0ObM4AZ22pTVIZdxhsbpHCyiIOtjbmA8jKRkEqQL3+yo9AQSAJgTzu9l1CjSY1WLPOvl71B5bi9ZpFE/FhKahuqyP9Lf+aHV9H3bhThaPrWTrLO1HjxMbaxWjTzgoJVzGqdbviQdfYzxcHTe4nAzBNxKHysAm00SGl8xaVviEzb4Gs5sN1cpRrbso2TjO/cvlfvX+9g==";
+
+    
 }
