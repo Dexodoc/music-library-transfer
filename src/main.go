63ffcb08c63bab6e059655c0d819590ac1b868b1
Prem Jagadeesh <prem.jagadeesh1@gmail.com>
2022-07-22 18:28:28 +0200

    spotify changes

--- src/main.go
+++ src/main.go
@@ -6,9 +6,13 @@ import (
 	"strconv"
 )
 
+
 func main() {
 
-	appleDeveloperToken := "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRSVUM3NzdEWkwifQ.eyJpYXQiOjE2NTgzMzYzNzksImV4cCI6MTY3Mzg4ODM3OSwiaXNzIjoiM1M1OVVGV1A4WSJ9.ywmXqlzN700OszGHLXePHOk2ifJ3EFWk_mF5XZT8w9bPgK9epenRO62tbpjdVMNgu6p6Hio6095Wig73_3xtwA"
+	os.Setenv("SPOTIFY_ID", "c8a2b5d8d88449f5b936603275a4f3fe")
+	os.Setenv("SPOTIFY_SECRET ", "afc789be90034389b8a0ab2d0a845aa2")
+
+	if false{appleDeveloperToken := "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRSVUM3NzdEWkwifQ.eyJpYXQiOjE2NTgzMzYzNzksImV4cCI6MTY3Mzg4ODM3OSwiaXNzIjoiM1M1OVVGV1A4WSJ9.ywmXqlzN700OszGHLXePHOk2ifJ3EFWk_mF5XZT8w9bPgK9epenRO62tbpjdVMNgu6p6Hio6095Wig73_3xtwA"
 	
 	// Currently personal token
 	musicUserToken := "AmIKSqcLaVo8ziS52+UVu6Qf+OW8vg8qXAhbpRi/+eh0ObM4AZ22pTVIZdxhsbpHCyiIOtjbmA8jKRkEqQL3+yo9AQSAJgTzu9l1CjSY1WLPOvl71B5bi9ZpFE/FhKahuqyP9Lf+aHV9H3bhThaPrWTrLO1HjxMbaxWjTzgoJVzGqdbviQdfYzxcHTe4nAzBNxKHysAm00SGl8xaVviEzb4Gs5sN1cpRrbso2TjO/cvlfvX+9g=="
@@ -27,5 +31,7 @@ func main() {
             f.WriteString(strconv.Itoa(offset + i) + " " + v.Attributes.ArtistName + " - " + v.Attributes.Name + "\n")
         }
 
-	}
+	}}
+
+	runSpotify()
 }
--- src/spotify.go
+++ src/spotify.go
@@ -1,31 +1,113 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
+	"math/rand"
+	"fmt"
+	"log"
 	"net/http"
-	"strconv"
+	"os"
+	"github.com/zmb3/spotify"
 )
 
- func spotify() {
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "https://api.music.apple.com/v1/me/library/songs", nil)
+type SpotifyClientBuilderConfig struct {
+	ClientID     string
+	ClientSecret string
+	Scopes       []string
+	LocalPort    string
+}
 
-	// Setting headers for user and developer authentication
-    	
+// SpotifyClientBuilder builds an authenticated Spotify client
+type SpotifyClientBuilder struct {
+	Config *SpotifyClientBuilderConfig
+	auth   spotify.Authenticator
+	state  string
+	ch     chan *spotify.Client
+}
 
-    req.Header.Set("Music-User-Token", userToken)
+func NewSpotifyClientBuilder(config *SpotifyClientBuilderConfig) *SpotifyClientBuilder {
+	c := &SpotifyClientBuilder{}
+	if config == nil {
+		c.Config = &SpotifyClientBuilderConfig{
+			Scopes:       []string{spotify.ScopeUserTopRead},
+			LocalPort:    "8080",
+			ClientID:     os.Getenv("SPOTIFY_ID"),
+			ClientSecret: os.Getenv("SPOTIFY_SECRET"),
+		}
+	} else {
+		c.Config = config
+	}
+	c.auth = spotify.NewAuthenticator(fmt.Sprintf("http://localhost:8080/callback"), c.Config.Scopes...)
+	if c.Config.ClientID != "" && c.Config.ClientSecret != "" {
+		c.auth.SetAuthInfo(c.Config.ClientID, c.Config.ClientSecret)
+	}
+	c.state = randStringBytes(40)
+	c.ch = make(chan *spotify.Client)
+	return c
+}
 
-	q := req.URL.Query()
-	q.Add("limit", strconv.Itoa(limit))
-	q.Add("offset", strconv.Itoa(offset))
-	req.URL.RawQuery = q.Encode()
+// GetClient uses the oauth2 flow to get an authenticated Spotify client
+func (c *SpotifyClientBuilder) GetClient() (*spotify.Client, error) {
+	// start an HTTP server and initiate an oidc flow
+	http.HandleFunc("/callback", c.completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go http.ListenAndServe(":8080", nil)
+	url := c.auth.AuthURL(c.state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+	// wait for auth to complete
+	client := <-c.ch
+	return client, nil
+}
 
-	res, _ := client.Do(req)
-	body, _ := ioutil.ReadAll(res.Body)
-	json.Unmarshal(body, &r)
+func (c *SpotifyClientBuilder) completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := c.auth.Token(c.state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != c.state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, c.state)
+	}
+	// use the token to get an authenticated client
+	client := c.auth.NewClient(tok)
+	fmt.Fprintf(w, "Login Completed!")
+	c.ch <- &client
+}
+
+func randStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789_"
+
+func runSpotify() {
+	spotifyClientBuilder := NewSpotifyClientBuilder(nil)
+	spotifyClient, err := spotifyClientBuilder.GetClient()
 	
-       
-
+	if err != nil {
+		log.Fatal(err)
+	}
+	// get the logged in user
+	user, err := spotifyClient.CurrentUser()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Hello " + user.DisplayName)
+	// get the user's top tracks
+	tracks, err := spotifyClient.CurrentUsersTopTracks()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := 0; i < len(tracks.Tracks); i++ {
+		fmt.Println(tracks.Tracks[i].Name)
+	}
+	
+    
 }
 
