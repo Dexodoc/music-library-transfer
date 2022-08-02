9482726082a688c0054477e46e8fa6952332fa03
Prem Jagadeesh <prem.jagadeesh1@gmail.com>
2022-08-02 18:32:06 +0200

    search apple music

--- src/main.go
+++ src/main.go
@@ -10,22 +10,32 @@ type SongList struct {
 }
 
 type Song struct {
-	name string
+	name    string
 	artists []string
 }
 
-func (sl *SongList) AddItem(song Song) []Song{
+func (sl *SongList) AddItem(song Song) []Song {
 	sl.Songs = append(sl.Songs, song)
 	return sl.Songs
 }
-
 
 func main() {
 	os.Setenv("SPOTIFY_ID", "c8a2b5d8d88449f5b936603275a4f3fe")
 	os.Setenv("SPOTIFY_SECRET", "afc789be90034389b8a0ab2d0a845aa2")
 	os.Setenv("APPLE_DEV_TOKEN", "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRSVUM3NzdEWkwifQ.eyJpYXQiOjE2NTkzOTYxMDMsImV4cCI6MTY3NDk0ODEwMywiaXNzIjoiM1M1OVVGV1A4WSJ9.Z5UJwcOyil_2DUjqmCP1ztCYCNnfa3RpwkYrf5rmbJEcgf0iNuzYOgltHUZdtASsxBlYVoI5be3ZW2OchAEEBg")
-	
+
 	pain := getMusicUserToken()
 	fmt.Println(pain)
-	fmt.Println(getSongCountApple(os.Getenv("APPLE_DEV_TOKEN"), pain))
+	fmt.Println(getStorefrontApple(os.Getenv("APPLE_DEV_TOKEN"), pain))
+
+	// client, _ := NewSpotifyClientBuilder().GetClient()
+	// song := Song{name: "Luck Be a Lady", artists: []string{"Frank Sinatra"}}
+	// song2 := Song{name: "We Don't Know", artists: []string{"The Strumbellas"}}
+	// song3 := Song{name: "Spirits", artists: []string{"The Strumbellas"}}
+	// song4 := Song{name: "Just Hold On", artists: []string{"Steve Aoki"}}
+	// song5 := Song{name: "Too Young to Burn", artists: []string{"Sonny & The Sunsets"}}
+	// songs := SongList{Songs: []Song{song, song2, song3, song4, song5}}
+	// addAllSongsSpotify(*client, songs)
+	// // addSongIdSpotify(*client, id)
+	// fmt.Println("ADAW")
 }
