015bdeb32949e6f53c9b782ce5e9727408d6c320
Prem Jagadeesh <prem.jagadeesh1@gmail.com>
2022-08-04 23:17:14 +0200

    remove developer tokens

--- src/jwt/generate_jwt.js
+++ src/jwt/generate_jwt.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 const jwt = require("jsonwebtoken");
 
 const privateKey = fs.readFileSync("AuthKey.p8").toString();
-const teamId = "3S59UFWP8Y";
-const keyId = "TRUC777DZL";
+const teamId = "";
+const keyId = "";
 
 const jwtToken = jwt.sign({}, privateKey, {
   algorithm: "ES256",
