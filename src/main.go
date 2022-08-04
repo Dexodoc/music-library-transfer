29b27a3a9f8ef89b6bf30a232847c6c3dc8cbf33
Prem Jagadeesh <prem.jagadeesh1@gmail.com>
2022-08-04 23:01:04 +0200

    change files

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type SongList struct {
@@ -20,23 +21,41 @@ func (sl *SongList) AddItem(song Song) []Song {
 }
 
 func main() {
-	os.Setenv("SPOTIFY_ID", "c8a2b5d8d88449f5b936603275a4f3fe")
-	os.Setenv("SPOTIFY_SECRET", "afc789be90034389b8a0ab2d0a845aa2")
-	os.Setenv("APPLE_DEV_TOKEN", "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRSVUM3NzdEWkwifQ.eyJpYXQiOjE2NTkzOTYxMDMsImV4cCI6MTY3NDk0ODEwMywiaXNzIjoiM1M1OVVGV1A4WSJ9.Z5UJwcOyil_2DUjqmCP1ztCYCNnfa3RpwkYrf5rmbJEcgf0iNuzYOgltHUZdtASsxBlYVoI5be3ZW2OchAEEBg")
+	// Need to set spotify credentials based on spotify app created in dashboard
+	// and ensure redirect uri is et for "http://localhost:8080/callback"
+	os.Setenv("SPOTIFY_ID", "")
+	os.Setenv("SPOTIFY_SECRET", "")
 
+	// Need to set JWT token generated from apple developer account
+	APPLE_DEV_TOKEN := ""
+
+	fmt.Println("Music Library Transfer")
+
+	fmt.Println("Would you like to transfer from Apple Music to Spotify(1) or Spotify to Apple Music(2):")
+	direction := getUserInputNumber()
 	
-	client, _ := NewSpotifyClientBuilder().GetClient()
-	songs := getAllSongsSpotify(*client)
+	spotifyClient, _ := NewSpotifyClientBuilder().GetClient()
+	appleUserToken := getMusicUserToken()
 
-	pain := getMusicUserToken()
-	addAllSongsApple(os.Getenv("APPLE_DEV_TOKEN"), pain, songs)
-	// song := Song{name: "Luck Be a Lady", artists: []string{"Frank Sinatra"}}
-	// song2 := Song{name: "We Don't Know", artists: []string{"The Strumbellas"}}
-	// song3 := Song{name: "Spirits", artists: []string{"The Strumbellas"}}
-	// song4 := Song{name: "Just Hold On", artists: []string{"Steve Aoki"}}
-	// song5 := Song{name: "Too Young to Burn", artists: []string{"Sonny & The Sunsets"}}
-	// songs := SongList{Songs: []Song{song, song2, song3, song4, song5}}
-	// addAllSongsSpotify(*client, songs)
-	// // addSongIdSpotify(*client, id)
-	fmt.Println("ADAW")
+	if direction == 1{
+		songs := getAllSongsApple(APPLE_DEV_TOKEN, appleUserToken)
+		addAllSongsSpotify(*spotifyClient, songs)
+	}else{
+		songs := getAllSongsSpotify(*spotifyClient)
+		addAllSongsApple(APPLE_DEV_TOKEN, appleUserToken, songs)
+	}
+}
+
+func getUserInputNumber() int {
+	var s string
+	var i int
+	for {
+		_, err := fmt.Scan(&s)
+		i, err = strconv.Atoi(s)
+		if err != nil || i > 2 || i < 1 {
+			fmt.Println("Enter a valid number")
+		} else {
+			return i
+		}
+	}
 }
